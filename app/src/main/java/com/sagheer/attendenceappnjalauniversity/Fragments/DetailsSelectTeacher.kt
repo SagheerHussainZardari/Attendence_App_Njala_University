c549df632fa06f7e19cad43b50765f48b3797efa
Sagheer Hussain Zardari <sagheerhzardari@gmail.com>
2020-05-14 16:39:01 +0500

    getting teachers and programs list from firebase

--- app/src/main/java/com/sagheer/attendenceappnjalauniversity/Fragments/DetailsSelectTeacher.kt
+++ app/src/main/java/com/sagheer/attendenceappnjalauniversity/Fragments/DetailsSelectTeacher.kt
@@ -5,13 +5,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.sagheer.attendenceappnjalauniversity.R
+import com.sagheer.attendenceappnjalauniversity.showToast
+import kotlinx.android.synthetic.main.fragment_details_select_teacher.*
+import org.json.JSONObject
 
 /**
  * A simple [Fragment] subclass.
  */
 class DetailsSelectTeacher : Fragment() {
+    private var listOfTeachersNames = ArrayList<String>()
+    private var listOfPrograms = ArrayList<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,5 +32,70 @@ class DetailsSelectTeacher : Fragment() {
         return inflater.inflate(R.layout.fragment_details_select_teacher, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        setTeachersDetails()
+
+
+    }
+
+    private fun setTeachersDetails() {
+        val urlTeachers = "https://njala-attendence.firebaseio.com/Teachers.json"
+        val urlProgramms = "https://njala-attendence.firebaseio.com/Programs.json"
+        val queue = Volley.newRequestQueue(context)
+
+        val srTeachers = StringRequest(Request.Method.GET, urlTeachers, Response.Listener {
+
+            for (key in JSONObject(it).keys()) {
+                listOfTeachersNames.add(JSONObject(it).getJSONObject(key).getString("name"))
+            }
+
+            at_TeacherName_TeacherDetailsFragment.setAdapter(
+                ArrayAdapter<String>(
+                    requireContext(),
+                    android.R.layout.simple_list_item_1,
+                    listOfTeachersNames
+                )
+            )
+        }, Response.ErrorListener {
+            context?.showToast("no responce")
+        })
+
+        val srPrograms = StringRequest(Request.Method.GET, urlProgramms, Response.Listener {
+            listOfPrograms.add(" Select Program")
+
+            for (key in JSONObject(it).keys()) {
+                listOfPrograms.add(JSONObject(it).getJSONObject(key).getString("name"))
+            }
+
+            spinner_Program_TeacherDetailsFragment.adapter = ArrayAdapter<String>(
+                requireContext(),
+                android.R.layout.simple_list_item_1,
+                listOfPrograms
+            )
+            setListnerForSpinnerPrograms()
+        }, Response.ErrorListener {
+            context?.showToast("no responce")
+        })
+
+        queue.add(srTeachers)
+        queue.add(srPrograms)
+    }
+
+    private fun setListnerForSpinnerPrograms() {
+        spinner_Program_TeacherDetailsFragment.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    context?.showToast(spinner_Program_TeacherDetailsFragment.selectedItem.toString())
+                }
+            }
+    }
 }
--- app/src/main/java/com/sagheer/attendenceappnjalauniversity/MainActivity.kt
+++ app/src/main/java/com/sagheer/attendenceappnjalauniversity/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.sagheer.attendenceappnjalauniversity
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
@@ -13,16 +12,15 @@ import com.google.firebase.auth.FirebaseAuth
 import com.sagheer.attendenceappnjalauniversity.Fragments.DetailsSelectStudent
 import com.sagheer.attendenceappnjalauniversity.Fragments.DetailsSelectTeacher
 import com.sagheer.attendenceappnjalauniversity.Fragments.Login
-import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
-
 
     companion object {
         var mAuth = FirebaseAuth.getInstance()
         var isTeacher = false
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,13 +31,8 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-
-
         if (mAuth.currentUser != null) {
             var isStudent = false
-
-            progressBarLayout.visibility = View.VISIBLE
-
             val url = "https://njala-attendence.firebaseio.com/Students.json"
             val queue = Volley.newRequestQueue(this)
             val sr = StringRequest(Request.Method.GET, url, Response.Listener {
@@ -51,7 +44,6 @@ class MainActivity : AppCompatActivity() {
                         isStudent = true
                     }
                 }
-                progressBarLayout.visibility = View.GONE
                 if (isStudent) {
                     openFragment(DetailsSelectStudent())
                 } else {
@@ -59,7 +51,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }, Response.ErrorListener {
-
+                showToast("No Internet Connection!!!")
             })
             queue.add(sr)
 
@@ -67,10 +59,6 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction().add(R.id.fragment_container, Login()).commit()
     }
 
-    override fun onBackPressed() {
-        if (mAuth.currentUser == null)
-            supportFragmentManager.popBackStack()
-    }
 
     fun openFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
