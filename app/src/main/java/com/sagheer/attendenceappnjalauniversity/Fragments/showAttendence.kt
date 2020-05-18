03db1c4fb5ef271bb941928a970aa2e8a61517f0
Sagheer Hussain Zardari <sagheerhzardari@gmail.com>
2020-05-18 10:02:26 +0500

    Getting Attendence Data Succefully

--- app/src/main/java/com/sagheer/attendenceappnjalauniversity/Fragments/showAttendence.kt
+++ app/src/main/java/com/sagheer/attendenceappnjalauniversity/Fragments/showAttendence.kt
@@ -7,14 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.sagheer.attendenceappnjalauniversity.AdaptersModels.ShowAttendenceAdapter
 import com.sagheer.attendenceappnjalauniversity.AdaptersModels.ShowAttendenceModel
+import com.sagheer.attendenceappnjalauniversity.Fragments.DetailsSelectStudent.Companion.course
+import com.sagheer.attendenceappnjalauniversity.Fragments.DetailsSelectStudent.Companion.program
+import com.sagheer.attendenceappnjalauniversity.Fragments.DetailsSelectStudent.Companion.year
 import com.sagheer.attendenceappnjalauniversity.R
+import com.sagheer.attendenceappnjalauniversity.showToast
 import kotlinx.android.synthetic.main.fragment_show_attendence.*
+import org.json.JSONObject
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class showAttendence : Fragment() {
 
     override fun onCreateView(
@@ -25,36 +32,51 @@ class showAttendence : Fragment() {
         return inflater.inflate(R.layout.fragment_show_attendence, container, false)
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         var list = ArrayList<ShowAttendenceModel>()
-        list.add(
-            ShowAttendenceModel(
-                "Sagheer-1234",
-                "3",
-                "2",
-                "1",
-                "6",
-                "50%"
-            )
-        )
-        list.add(
-            ShowAttendenceModel(
-                "Sagheer-1234",
-                "3",
-                "2",
-                "1",
-                "6",
-                "50%"
-            )
-        )
+        var url =
+            "https://njala-attendence.firebaseio.com/StudentsAttedence/$program/$year/$course.json"
+        var queue = Volley.newRequestQueue(context)
 
-        rc_showAttendence.setHasFixedSize(true)
-        rc_showAttendence.layoutManager = LinearLayoutManager(context)
-        rc_showAttendence.adapter = ShowAttendenceAdapter(requireContext(), list)
+        var sr = StringRequest(Request.Method.GET, url, Response.Listener {
+
+            if (it != "null") {
+
+                for (key in JSONObject(it).keys()) {
+                    var nameRoll =
+                        JSONObject(it).getJSONObject(key).getString("name").toString() + "-" + key.toString()
+                    var present = JSONObject(it).getJSONObject(key).getString("present").toInt()
+                    var absent = JSONObject(it).getJSONObject(key).getString("absent").toInt()
+                    var sick = JSONObject(it).getJSONObject(key).getString("sick").toInt()
+                    var total = present + absent + sick
+                    var percentage = (present * 100) / total
+                    list.add(
+                        ShowAttendenceModel(
+                            nameRoll,
+                            present.toString(),
+                            absent.toString(),
+                            sick.toString(),
+                            total.toString(),
+                            "%$percentage"
+
+                        )
+                    )
+                }
+
+                rc_showAttendence.setHasFixedSize(true)
+                rc_showAttendence.layoutManager = LinearLayoutManager(context)
+                rc_showAttendence.adapter = ShowAttendenceAdapter(requireContext(), list)
+
+            } else {
+                context?.showToast("No Data Found!!!")
+            }
+
+        }, Response.ErrorListener {
+            context?.showToast("Check Your Internet Connection!")
+        })
+
+        queue.add(sr)
+
     }
-
-
 }
